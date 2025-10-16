687ff17096fb48a9035717059194e9704862ceef
thandozulu <tzulu49@gmail.com>
2025-10-16 23:02:35 +0200

    Initial commit

---
+++ src/main/java/com/example/bank_transactions/BankTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bank_transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransactionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bank_transactions/BankTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bank_transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransactionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
